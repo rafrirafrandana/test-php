76e3161bf075c3c6729930372f266e797f38ba20
Rafri Rafrandana <89951083+rafrirafrandana@users.noreply.github.com>
2022-08-10 10:20:47 +0700

    Update 1.php

--- August 10, 2022 - Class & Function/1.php
+++ August 10, 2022 - Class & Function/1.php
@@ -1,1 +1,24 @@
+<?php
+    class cpu {
+        var $processor;
+        var $ram;
+        var $vga_card;
+        var $harddisk;
+        var $motherboard;
+        var $power_supply;
+        var $cooling_fan;
+        var $casing_cpu;
+                
+        function cpuInput($processor){
+            return "Hello Andi & Bobi!";
+        }
+    }
 
+    $cpuA = new cpu();
+    $cpuB = new cpu();
+            
+    $cpuA->name = "Andi";
+    $cpuB->name = "Bobi";
+
+    echo $cpuA->name . "<br>" . $cpuB->name;
+?>
